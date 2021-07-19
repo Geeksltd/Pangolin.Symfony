737305e20c64ed4f600298b64479a99ef92ffbad
Vahid Mahdiun <vahid.mahdiun@geeks.ltd.uk>
2021-07-19 18:15:17 +0100

    Removed extra method

--- src/PangolinBundle.php
+++ src/PangolinBundle.php
@@ -4,16 +4,8 @@ namespace Geeks\Pangolin;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class PangolinBundle extends Bundle
 {
-
-    public function load(array $configs, ContainerBuilder $container)
-    {
-
-    }
-
-
 
 }
