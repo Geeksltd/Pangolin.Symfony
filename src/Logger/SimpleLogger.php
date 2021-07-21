0d60aed160ac58a2e56e91c9e6e5ce8560f12ddc
Vahid Mahdiun <vahid.mahdiun@geeks.ltd.uk>
2021-07-21 19:17:41 +0100

    Implemented a custom logger for saving sql logs

---
+++ src/Logger/SimpleLogger.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Geeks\Pangolin\Logger;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\DBAL\Logging\SQLLogger;
+use Doctrine\DBAL\Types\Type;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+
+class SimpleLogger implements SQLLogger
+{
+
+    protected $types = [
+        "UPDATE", "INSERT", "CREATE", "ALTER"
+        // "SELECT", "DELETE"
+    ];
+    protected $em;
+    protected $cache;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+        $this->cache = new FilesystemAdapter();
+    }
+
+
+    public function startQuery($sql, array $params = null, array $types = null)
+    {
+        $platform = $this->em->getConnection()->getDatabasePlatform();
+        if ($this->isLoggable($sql, $this->types, true)) {
+            if (!empty($params)) {
+                foreach ($params as $key => $param) {
+                    $type = Type::getType($types[$key]);
+                    $value = $type->convertToDatabaseValue($param, $platform);
+                    $sql = join(var_export($value, true), explode('?', $sql, 2));
+                }
+                $logItem = $this->cache->getItem('all_cached_logs');
+                $newValue = $logItem->get(). "\n". $sql;
+                $logItem->set($newValue);
+                $this->cache->save($logItem);
+            }
+        }
+
+    }
+
+    protected function isLoggable($string, array $search, $caseInsensitive = false)
+    {
+        $exp = '/' . implode('|',
+                array_map('preg_quote', $search))
+            . ($caseInsensitive ? '/i' : '/');
+        return preg_match($exp, $string);
+    }
+
+
+    public function stopQuery()
+    {
+    }
+
+
+}
