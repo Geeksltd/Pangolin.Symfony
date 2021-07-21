993e2806a3f603a78155de9eb300adbce5fbc28c
Vahid Mahdiun <vahid.mahdiun@geeks.ltd.uk>
2021-07-21 19:18:00 +0100

    Created a new controller

---
+++ src/Controller/LogController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Geeks\Pangolin\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+
+class LogController extends AbstractController
+{
+
+    /**
+     * @Route("/cmd/logs", name="pangolin_logs")
+     */
+    public function index(KernelInterface $kernel): Response
+    {
+        $cache = new FilesystemAdapter();
+        $logs = $cache->get('all_cached_logs', function ($item){
+            return $item->get();
+        });
+        $cache->delete("all_cached_logs");
+        return $this->json([
+            'message' => $logs,
+            'status' => true
+        ]);
+    }
+
+}
