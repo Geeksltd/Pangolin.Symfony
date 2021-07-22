8dfbdb61bdb0901f041c74dc0c65c8f78498ac3f
Vahid Mahdiun <vahid.mahdiun@geeks.ltd.uk>
2021-07-22 17:48:13 +0100

    Updated config files for new route and service

---
+++ src/Controller/SqlExecutionController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Geeks\Pangolin\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class SqlExecutionController extends AbstractController
+{
+
+    /**
+     * @Route("/cmd/db-run-changes", name="pangolin_sql_execution")
+     */
+    public function index(): Response
+    {
+
+
+        $postData = file_get_contents('php://input');
+
+        $xml = simplexml_load_string($postData);
+
+        $value = $xml->attributes()->DataBaseCommand[0];
+
+        $string = $value->__toString();
+        $decodedString = json_decode($string);
+        
+        $queries = $decodedString->data;
+
+        foreach ($queries as $sql){
+            $this->executeSqlQuery($sql->log);
+        }
+
+    }
+
+
+    protected function executeSqlQuery($sql)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $stmt = $em->getConnection()->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+}
