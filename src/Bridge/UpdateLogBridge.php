b3836fa09dfed04b7d7d722da200d78853a6d1be
Vahid Mahdiun <vahid.mahdiun@geeks.ltd.uk>
2021-09-13 13:29:38 +0100

    Removed return type from get id

--- src/Bridge/UpdateLogBridge.php
+++ src/Bridge/UpdateLogBridge.php
@@ -3,11 +3,10 @@
 
 namespace Geeks\Pangolin\Bridge;
 
-use Doctrine\DBAL\Types\GuidType;
-
+//use Doctrine\DBAL\Types\GuidType;
 
 interface UpdateLogBridge
 {
-    public function getId(): GuidType;
+    public function getId();
     public function setId($id): void;
 }
