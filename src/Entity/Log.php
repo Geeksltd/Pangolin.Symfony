a73b64f604202863a58da20968f36039c4e858f7
atir_naveed_geeksltd <atir.naveed@geeks.ltd.uk>
2022-10-06 13:44:56 +0100

    update new symfony version

--- src/Entity/Log.php
+++ src/Entity/Log.php
@@ -2,7 +2,11 @@
 
 namespace Geeks\Pangolin\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiFilter;
 use Geeks\Pangolin\Repository\LogRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Geeks\Pangolin\Controller\LogController;
@@ -10,71 +14,10 @@ use Geeks\Pangolin\Controller\ResetDatabaseController;
 use Geeks\Pangolin\Dto\LogInput;
 use Geeks\Pangolin\Controller\SqlExecutionController;
 use Geeks\Pangolin\Controller\LocaltimeController;
-
 /**
- * @ApiResource(
- *     attributes={"pagination_enabled"=false},
- *     input=LogInput::class,
- *     collectionOperations={
- *          "get_logs"= {
- *            "method"="GET",
- *            "path"="/get-db-changes",
- *            "controller"=LogController::class,
- *          },
- *          "time_update"= {
- *            "method"="GET",
- *            "path"="/local-date",
- *            "controller"=LocaltimeController::class,
- *            "openapi_context" = {
- *                  "parameters" = {
- *                      {
- *                          "name" = "date",
- *                          "in" = "query",
- *                          "required" : true,
- *                          "description" = "",
- *                          "schema" = {
- *                              "type" = "string"
- *                          }
- *                      },
- *                      {
- *                          "name" = "time",
- *                          "in" = "query",
- *                          "description" = "",
- *                          "schema" = {
- *                              "type" = "string"
- *                          }
- *                      }
- *                  }
- *               }
- *          },
- *          "get_db_reset"= {
- *            "method"="GET",
- *            "path"="/db-restart",
- *            "controller"=ResetDatabaseController::class,
- *           },
- *          "get_db_run_changes"= {
- *            "method"="POST",
- *            "path"="/db-run-changes",
- *            "controller"=SqlExecutionController::class,
- *             "openapi_context"={
- *                 "requestBody"={
- *                     "content"={
- *                         "application/json"={
- *                             "schema"={
- *
- *                             }
- *                         }
- *                     }
- *                 }
- *             }
-
- *           },
- *     },
- *     itemOperations={
- *     }
- * )
- * @ORM\Entity(repositoryClass=LogRepository::class)
+ * @ORM\Entity (repositoryClass=LogRepository::class)
  */
+#[ApiResource(operations: [new GetCollection(uriTemplate: '/get-db-changes', controller: LogController::class), new GetCollection(uriTemplate: '/local-date', controller: LocaltimeController::class, openapiContext: ['parameters' => [['name' => 'date', 'in' => 'query', 'required' => true, 'description' => '', 'schema' => ['type' => 'string']], ['name' => 'time', 'in' => 'query', 'description' => '', 'schema' => ['type' => 'string']]]]), new GetCollection(uriTemplate: '/db-restart', controller: ResetDatabaseController::class), new Post(uriTemplate: '/db-run-changes', controller: SqlExecutionController::class, openapiContext: ['requestBody' => ['content' => ['application/json' => ['schema' => []]]]])], paginationEnabled: false, input: LogInput::class)]
 class Log
 {
     /**
@@ -83,128 +26,99 @@ class Log
      * @ORM\Column(type="integer")
      */
     private $id;
-
     /**
      * @ORM\Column(type="string", length=255)
      */
     private $typeName;
-
     /**
      * @ORM\Column(type="datetime_immutable", nullable=true)
      */
     private $updatedAt;
-
     /**
      * @ORM\Column(type="datetime_immutable")
      */
     private $createdAt;
-
     /**
      * @ORM\Column(type="text", nullable=true)
      */
     private $payload;
-
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $columnId;
-
     /**
      * @ORM\Column(type="string", length=255)
      */
     private $actionName;
-
     /**
      * @ORM\Column(type="text", nullable=true)
      */
     private $dbalQuery;
-
-    public function getId(): ?int
+    public function getId() : ?int
     {
         return $this->id;
     }
-
-    public function getTypeName(): ?string
+    public function getTypeName() : ?string
     {
         return $this->typeName;
     }
-
-    public function setTypeName(string $typeName): self
+    public function setTypeName(string $typeName) : self
     {
         $this->typeName = $typeName;
-
         return $this;
     }
-
-    public function getUpdatedAt(): ?\DateTimeImmutable
+    public function getUpdatedAt() : ?\DateTimeImmutable
     {
         return $this->updatedAt;
     }
-
-    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): self
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt) : self
     {
         $this->updatedAt = $updatedAt;
-
         return $this;
     }
-
-    public function getCreatedAt(): ?\DateTimeImmutable
+    public function getCreatedAt() : ?\DateTimeImmutable
     {
         return $this->createdAt;
     }
-
-    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    public function setCreatedAt(\DateTimeImmutable $createdAt) : self
     {
         $this->createdAt = $createdAt;
-
         return $this;
     }
-
-    public function getPayload(): ?string
+    public function getPayload() : ?string
     {
         return $this->payload;
     }
-
-    public function setPayload(string $payload): self
+    public function setPayload(string $payload) : self
     {
         $this->payload = $payload;
-
         return $this;
     }
-
     public function getColumnId()
     {
         return $this->columnId;
     }
-
-    public function setColumnId(string $columnId): self
+    public function setColumnId(string $columnId) : self
     {
         $this->columnId = $columnId;
-
         return $this;
     }
-
-    public function getActionName(): ?string
+    public function getActionName() : ?string
     {
         return $this->actionName;
     }
-
-    public function setActionName(string $actionName): self
+    public function setActionName(string $actionName) : self
     {
         $this->actionName = $actionName;
-
         return $this;
     }
-
-    public function getDbalQuery(): ?string
+    public function getDbalQuery() : ?string
     {
         return $this->dbalQuery;
     }
-
-    public function setDbalQuery(?string $dbalQuery): self
+    public function setDbalQuery(?string $dbalQuery) : self
     {
         $this->dbalQuery = $dbalQuery;
-
         return $this;
     }
 }
